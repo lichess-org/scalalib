7a1863b493f0916e8bdfba4bb2e1f00ad7a26554
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-20 12:42:31 +0100

    Fix DateTime trait name

--- src/main/scala/OrnicarDateTime.scala
+++ src/main/scala/OrnicarDateTime.scala
@@ -2,7 +2,7 @@ package ornicar.scalalib
 
 import org.joda.time.{ DateTime => JDateTime }
 
-trait DateTime {
+trait OrnicarDateTime {
 
   implicit def richDateTime(date: JDateTime) = new {
     def getSeconds: Int = math.round(date.getMillis / 1000)
