e2a15cfec014700cb7dbbe2ba8a3a3cc2a53f724
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-24 11:46:43 +0100

    Simplify validation

--- src/main/scala/OrnicarValidation.scala
+++ src/main/scala/OrnicarValidation.scala
@@ -1,47 +1,40 @@
 package ornicar.scalalib
 
 import util.control.Exception.allCatch
-import scalaz.{ Validation ⇒ ScalazValidation, Success, Failure, Semigroup, Apply, NonEmptyList }
+import scalaz.{ Validation, Success, Failure, Semigroup, Apply, NonEmptyList }
 
 trait OrnicarValidation
     extends scalaz.Validations
-    with scalaz.Semigroups
     with scalaz.Options
     with scalaz.MABs
     with scalaz.Identitys {
 
   type Failures = NonEmptyList[String]
 
-  type Valid[A] = ScalazValidation[Failures, A]
+  type Valid[A] = Validation[Failures, A]
 
   implicit def eitherToValidation[E, B](either: Either[E, B]): Valid[B] =
     validation(either.left map {
-      case e: Throwable       ⇒ throwableToFailures(e)
-      case s: String          ⇒ stringToFailures(s)
+      case e: Throwable       ⇒ e.getMessage wrapNel
       case m: NonEmptyList[_] ⇒ m map (_.toString)
+      case s                  ⇒ s.toString wrapNel
     })
 
-  implicit def stringToFailures(s: String): Failures = s wrapNel
-
-  implicit def throwableToFailures(e: Throwable): Failures = e.getMessage wrapNel
-
-  implicit def richStringToFailures(str: String) = new {
-    def toFailures: Failures = stringToFailures(str)
+  implicit def richString(str: String) = new {
+    def toFailures: Failures = str wrapNel
   }
 
-  implicit def richScalazValidation[E, A](validation: ScalazValidation[E, A]) = new {
+  implicit def richValidation[E, A](validation: Validation[E, A]) = new {
 
-    def and[B](f: ScalazValidation[E, A ⇒ B])
-    (implicit a: Apply[({ type λ[α] = ScalazValidation[E, α] })#λ])
-    : ScalazValidation[E, B] = validation <*> f
+    def and[B](f: Validation[E, A ⇒ B])(implicit a: Apply[({ type λ[α] = Validation[E, α] })#λ]): Validation[E, B] = validation <*> f
 
-    def mapFail[F](f: E => F): ScalazValidation[F, A] = validation match {
-      case Success(s) => Success(s)
-      case Failure(s) => Failure(f(s))
+    def mapFail[F](f: E ⇒ F): Validation[F, A] = validation match {
+      case Success(s) ⇒ Success(s)
+      case Failure(s) ⇒ Failure(f(s))
     }
   }
 
-  def unsafe[A](op: ⇒ A)(implicit handle: Throwable ⇒ Failures = throwableToFailures): Valid[A] =
+  def unsafe[A](op: ⇒ A)(implicit handle: Throwable ⇒ String = _.getMessage): Valid[A] =
     eitherToValidation((allCatch either op).left map handle)
 
   def validateOption[A, B](ao: Option[A])(op: A ⇒ Valid[B]): Valid[Option[B]] =
