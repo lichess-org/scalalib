d8c0438ba938b16452ea6b05833d68ede7bd9c98
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-27 16:24:10 +0100

    version 1.9 published on scala.iliaz.com

--- build.sbt
+++
@@ -1,6 +0,0 @@
-libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "6.0.4",
-  "org.specs2" %% "specs2" % "1.8.2",
-  "joda-time" % "joda-time" % "2.0",
-  "org.joda" % "joda-convert" % "1.2"
-)
---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+
+object ScalalibBuild extends Build {
+
+  lazy val core = Project("core", file(".")) settings (
+    organization := "com.github.ornicar",
+    name := "scalalib",
+    version := "1.9",
+    scalaVersion := "2.9.1",
+    libraryDependencies ++= Seq(
+      "org.scalaz" %% "scalaz-core" % "6.0.4",
+      "org.specs2" %% "specs2" % "1.8.2",
+      "joda-time" % "joda-time" % "2.0",
+      "org.joda" % "joda-convert" % "1.2"
+    ),
+    scalacOptions := Seq("-deprecation", "-unchecked"),
+    publishTo := Some(Resolver.sftp(
+      "iliaz",
+      "scala.iliaz.com"
+    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa"))
+  )
+}
