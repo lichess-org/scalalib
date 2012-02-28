8d29b240d9740069c55ce2ce405cd0e82cd1a7d5
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-28 17:09:21 +0100

    Bump version 1.11

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object ScalalibBuild extends Build {
   lazy val core = Project("core", file(".")) settings (
     organization := "com.github.ornicar",
     name := "scalalib",
-    version := "1.10",
+    version := "1.11",
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
       "org.scalaz" %% "scalaz-core" % "6.0.4",
