88c5b2896e4bcc8fd4856a396eaaa9fb3627c952
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-23 19:33:10 +0200

    Add random approximations

--- src/main/scala/OrnicarRandom.scala
+++ src/main/scala/OrnicarRandom.scala
@@ -1,10 +1,23 @@
 package ornicar.scalalib
 
 import scala.util.Random
+import scala.math.round
 
 object OrnicarRandom {
 
   def nextAsciiString(len: Int) = List.fill(len)(nextAsciiChar) mkString
 
-  private def nextAsciiChar = (Random.nextInt(25) + 97).toChar
+  def nextAsciiChar = (Random.nextInt(25) + 97).toChar
+
+  def approximatly(ratio: Float = 0.1f) = new {
+
+    def apply(number: Double): Double =
+      number + (ratio * number * 2 * Random.nextDouble) - (ratio * number)
+
+    def apply(number: Float): Float =
+      apply(number.toDouble).toFloat
+
+    def apply(number: Int): Int =
+      round(apply(number.toFloat))
+  }
 }
