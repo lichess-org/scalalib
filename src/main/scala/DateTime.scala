c1a387b2f3d1535235c29e87241d095ecb65cd1c
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-08 12:04:27 +0200

    Provide Ordering[DateTime]

--- src/main/scala/DateTime.scala
+++ src/main/scala/DateTime.scala
@@ -1,6 +1,6 @@
 package ornicar.scalalib
 
-import org.joda.time.{ DateTime => JodaDateTime }
+import org.joda.time.{ DateTime ⇒ JodaDateTime }
 import java.util.Date
 
 trait DateTime {
@@ -9,4 +9,10 @@ trait DateTime {
     def getSeconds: Long = date.getMillis / 1000
     def getDate: Date = date.toDate
   }
+
+  implicit def dateTimeOrdering: Ordering[JodaDateTime] =
+    new Ordering[JodaDateTime] {
+      def compare(a: JodaDateTime, b: JodaDateTime) =
+        org.joda.time.DateTimeComparator.getInstance.compare(a, b)
+    }
 }
