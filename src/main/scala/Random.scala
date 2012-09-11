2ee1011e1c124603cc92fe3bfcbafedec5732e9e
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-11 14:56:11 +0200

    Rename Random object

--- src/main/scala/Random.scala
+++ src/main/scala/Random.scala
@@ -1,21 +1,21 @@
 package ornicar.scalalib
 
-import scala.util.Random
+import scala.util.{ Random => ScalaRandom }
 import scala.math.round
 
-object OrnicarRandom {
+object Random {
 
   private val chars: IndexedSeq[Char] = (('0' to '9') ++ ('a' to 'z'))
   private val nbChars = chars.size
 
   def nextString(len: Int) = List.fill(len)(nextChar) mkString
 
-  def nextChar = chars(Random nextInt nbChars)
+  def nextChar = chars(ScalaRandom nextInt nbChars)
 
   def approximatly(ratio: Float = 0.1f) = new {
 
     def apply(number: Double): Double =
-      number + (ratio * number * 2 * Random.nextDouble) - (ratio * number)
+      number + (ratio * number * 2 * ScalaRandom.nextDouble) - (ratio * number)
 
     def apply(number: Float): Float =
       apply(number.toDouble).toFloat
