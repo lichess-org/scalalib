998e87ea83e4f6eb645a6b80ef724fef931b51f9
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-11-29 22:04:39 +0100

    add IO functions

--- src/main/scala/IO.scala
+++ src/main/scala/IO.scala
@@ -1,8 +1,9 @@
 package ornicar.scalalib
 
 import scalaz.effects.{ IO ⇒ SIO }
+import scalaz.Zero
 
-trait IO {
+trait IO extends scalaz.Zeros {
 
   implicit def ornicarRichIOUnit(iou: SIO[Unit]) = new {
 
@@ -11,5 +12,16 @@ trait IO {
     def doUnless(cond: Boolean): SIO[Unit] = if (cond) SIO.ioPure pure Unit else iou
 
     def inject[A](a: A): SIO[A] = iou map (_ ⇒ a)
+  }
+
+  implicit def ornicarRichIOA[A](ioa: SIO[A]) = new {
+
+    def void: SIO[Unit] = ioa map (_ ⇒ Unit)
+  }
+
+  val void: SIO[Unit] = SIO.ioPure pure Unit
+
+  implicit def IOZero[A: Zero]: Zero[SIO[A]] = new Zero[SIO[A]] {
+    val zero = SIO.ioPure pure ∅[A]
   }
 }
