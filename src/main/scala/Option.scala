0f073e2bf71cbc8695c5be24f7b4fb6bfe55b264
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 22:03:20 +0200

    more option functions

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -5,6 +5,11 @@ trait OrnicarOption[A] {
   def self: Option[A]
 
   final def unary_~(implicit z: Zero[A]): A = self getOrElse z.zero
+
+  def ??[B: Zero](f: A ⇒ B): B = self.fold(Zero[B].zero)(f)
+
+  def ifTrue(b: Boolean): Option[A] = self filter (_ ⇒ b)
+  def ifFalse(b: Boolean): Option[A] = self filter (_ ⇒ !b)
 }
 
 object OrnicarOption {
