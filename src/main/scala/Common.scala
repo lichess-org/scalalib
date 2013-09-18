47c273e4689c1299851afadb03fc6dda4b110fac
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 16:41:08 +0200

    implicit final classes

--- src/main/scala/Common.scala
+++ src/main/scala/Common.scala
@@ -15,7 +15,7 @@ trait Common {
    * Provides oneliner side effects
    * See http://hacking-scala.posterous.com/side-effecting-without-braces
    */
-  implicit def ornicarAddKcombinator[A](any: A) = new {
+  implicit final class ornicarAddKcombinator[A](any: A) {
     def kCombinator(sideEffect: A ⇒ Unit): A = {
       sideEffect(any)
       any
@@ -24,7 +24,7 @@ trait Common {
     def pp: A = kCombinator(println)
   }
 
-  implicit def ornicarRichMap[A, B](m: Map[A, B]) = new {
+  implicit final class ornicarRichMap[A, B](m: Map[A, B]) {
 
     // Add Map.mapKeys, similar to Map.mapValues
     def mapKeys[C](f: A ⇒ C): Map[C, B] = m map {
@@ -35,7 +35,7 @@ trait Common {
     def filterValues(p: B ⇒ Boolean): Map[A, B] = m filter { x ⇒ p(x._2) }
   }
 
-  implicit def ornicarRichIdentity[A](a: A) = new {
+  implicit final class ornicarRichIdentity[A](a: A) {
 
     def combine[B](o: Option[B])(f: (A, B) ⇒ A): A = o match {
       case None    ⇒ a
