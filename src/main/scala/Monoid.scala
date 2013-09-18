0f98356d5fc32d1dc8d45e8878c14166de13fc0a
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 21:41:17 +0200

    derive Zero from Monoid

--- src/main/scala/Monoid.scala
+++ src/main/scala/Monoid.scala
@@ -6,27 +6,25 @@ object OrnicarMonoid {
 
   trait Instances {
 
-    import Zero.Instances._
-    import Zero.Syntax._
-
     implicit val BooleanMonoid: Monoid[Boolean] = new Monoid[Boolean] {
       def append(f1: Boolean, f2: ⇒ Boolean) = f1 || f2
-      def zero = ∅[Boolean]
+      def zero = false
     }
 
     implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
       def append(f1: Int, f2: ⇒ Int) = f1 + f2
-      def zero = ∅[Int]
+      def zero = 0
     }
 
     implicit val UnitMonoid: Monoid[Unit] = new Monoid[Unit] {
       def append(f1: Unit, f2: ⇒ Unit) = ()
-      def zero = ∅[Unit]
+      def zero = ()
     }
 
     implicit def SeqMonoid[A]: Monoid[Seq[A]] = new Monoid[Seq[A]] {
       def append(f1: Seq[A], f2: ⇒ Seq[A]) = f1 ++ f2
-      def zero: Seq[A] = ∅[Seq[A]]
+      def zero: Seq[A] = Seq.empty
     }
   }
+  object Instances extends Instances
 }
--- src/main/scala/Zero.scala
+++ src/main/scala/Zero.scala
@@ -18,26 +18,15 @@ object Zero {
   def mzero[Z](implicit z: Zero[Z]): Z = z.zero
 
   trait Syntax {
+
     def ∅[F](implicit F: Zero[F]): F = F.zero
   }
-  object Syntax extends Syntax
 
   trait Instances {
 
-    implicit def SeqZero[A]: Zero[Seq[A]] = instance(Seq.empty[A])
-    implicit def ListZero[A]: Zero[List[A]] = instance(List.empty[A])
-    implicit def UnitZero: Zero[Unit] = instance(())
-    implicit def StringZero: Zero[String] = instance("")
-    implicit def IntZero: Zero[Int] = instance(0)
-    implicit def BooleanZero: Zero[Boolean] = instance(false)
-    implicit def CharZero: Zero[Char] = instance(0.toChar)
-    implicit def ByteZero: Zero[Byte] = instance(0.toByte)
-    implicit def LongZero: Zero[Long] = instance(0L)
-    implicit def ShortZero: Zero[Short] = instance(0.toShort)
-    implicit def FloatZero: Zero[Float] = instance(0F)
-    implicit def DoubleZero: Zero[Double] = instance(0D)
-    implicit def BigIntegerZero = instance(java.math.BigInteger.valueOf(0))
-    implicit def BigIntZero: Zero[BigInt] = instance(BigInt(0))
+    import scalaz.Monoid
+    import OrnicarMonoid.Instances._
+
+    implicit def MonoidZero[A](implicit m: scalaz.Monoid[A]): Zero[A] = instance(m.zero)
   }
-  object Instances extends Instances
 }
