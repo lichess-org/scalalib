ed1e9511c28832f1931486d42b9061507134ec25
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 15:05:16 +0200

    v4.0 with scalaz 7.0.3

--- project/Build.scala
+++ project/Build.scala
@@ -6,12 +6,12 @@ object ScalalibBuild extends Build {
   lazy val core = Project("core", file(".")) settings (
     organization := "com.github.ornicar",
     name := "scalalib",
-    version := "3.3",
-    scalaVersion := "2.10.0",
+    version := "4.0",
+    scalaVersion := "2.10.2",
     resolvers ++= Seq(
       "sonatype" at "http://oss.sonatype.org/content/repositories/releases"),
       libraryDependencies ++= Seq(
-        "org.scalaz" %% "scalaz-core" % "6.0.4",
+        "org.scalaz" %% "scalaz-core" % "7.0.3",
         "org.specs2" %% "specs2" % "1.14",
         "joda-time" % "joda-time" % "2.1",
         "org.joda" % "joda-convert" % "1.2"
