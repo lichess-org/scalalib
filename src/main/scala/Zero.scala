5444fbc85ad2635a45d6ea633c6944e042442103
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-05 02:07:43 +0100

    Add Zero Vector instance

--- src/main/scala/Zero.scala
+++ src/main/scala/Zero.scala
@@ -25,5 +25,6 @@ object Zero {
     implicit def MonoidZero[A](implicit m: scalaz.Monoid[A]): Zero[A] = instance(m.zero)
     implicit def OptionZero[A]: Zero[Option[A]] = instance(None)
     implicit def SetZero[A]: Zero[Set[A]] = instance(Set.empty)
+    implicit def VectorZero[A]: Zero[Vector[A]] = instance(Vector.empty)
   }
 }
