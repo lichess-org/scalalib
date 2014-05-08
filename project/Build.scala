b272931e5926fb87a4f6fb58552f53c78e942a27
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-08 12:02:49 +0200

    remove joda time

--- project/Build.scala
+++ project/Build.scala
@@ -1,21 +1,17 @@
-import sbt._
-import Keys._
+import sbt._, Keys._
 
 object ScalalibBuild extends Build {
 
   lazy val core = Project("core", file(".")) settings (
     organization := "com.github.ornicar",
     name := "scalalib",
-    version := "4.23",
+    version := "4.24",
     scalaVersion := "2.10.3",
     resolvers ++= Seq(
       "sonatype" at "http://oss.sonatype.org/content/repositories/releases"),
       libraryDependencies ++= Seq(
         "org.scalaz" %% "scalaz-core" % "7.0.4",
-        "org.specs2" %% "specs2" % "2.3.4",
-        "joda-time" % "joda-time" % "2.1",
-        "org.joda" % "joda-convert" % "1.2"
-      ),
+        "org.specs2" %% "specs2" % "2.3.4"),
         scalacOptions := Seq(
           "-deprecation",
           "-unchecked",
