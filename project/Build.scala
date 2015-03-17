9f21c2b30f1b7d65f0f2c784e8a130173f77c087
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-17 12:10:22 +0100

    upgrade stuff

--- project/Build.scala
+++ project/Build.scala
@@ -5,21 +5,19 @@ object ScalalibBuild extends Build {
   lazy val core = Project("core", file(".")) settings (
     organization := "com.github.ornicar",
     name := "scalalib",
-    version := "5.2",
-    scalaVersion := "2.11.5",
+    version := "5.3",
+    scalaVersion := "2.11.6",
     resolvers ++= Seq(
-      "sonatype" at "http://oss.sonatype.org/content/repositories/releases"),
+      "sonatype" at "http://oss.sonatype.org/content/repositories/releases",
+      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+    ),
       libraryDependencies ++= Seq(
-        "org.scalaz" %% "scalaz-core" % "7.1.0",
-        "org.specs2" %% "specs2" % "2.4.1"),
+        "org.scalaz" %% "scalaz-core" % "7.1.1",
+        "org.specs2" %% "specs2-core" % "3.0.1"),
         scalacOptions := Seq(
           "-deprecation",
           "-unchecked",
           "-feature",
-          "-language:_"),
-          publishTo := Some(Resolver.sftp(
-            "iliaz",
-            "scala.iliaz.com"
-          ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa"))
+          "-language:_")
   )
 }
