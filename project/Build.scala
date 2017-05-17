79c14b97f48ec5b0a6b29d80619bd74d419e7477
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-17 13:14:14 +0200

    better publish

--- project/Build.scala
+++ project/Build.scala
@@ -6,20 +6,20 @@ object ScalalibBuild extends Build {
     organization := "com.github.ornicar",
     name := "scalalib",
     version := "5.7",
-    scalaVersion := "2.11.8",
+    scalaVersion := "2.11.11",
     licenses += "MIT" -> url("http://opensource.org/licenses/MIT"),
     resolvers ++= Seq(
-      "sonatype" at "http://oss.sonatype.org/content/repositories/releases",
-      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+      "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
     ),
-      libraryDependencies ++= Seq(
-        "org.scalaz" %% "scalaz-core" % "7.1.11",
-        "org.specs2" %% "specs2-core" % "3.6"),
-        scalacOptions := Seq(
-          "-deprecation",
-          "-unchecked",
-          "-feature",
-          "-language:_",
-          "-Xfatal-warnings")
+    libraryDependencies ++= Seq(
+      "org.scalaz" %% "scalaz-core" % "7.1.11",
+      "org.specs2" %% "specs2-core" % "3.6"),
+    scalacOptions := Seq(
+      "-deprecation",
+      "-unchecked",
+      "-feature",
+      "-language:_",
+      "-Xfatal-warnings"),
+    publishTo := Some(Resolver.file("file",  new File(sys.props.getOrElse("publishTo", ""))))
   )
 }
