70f02be9075feceef75438e69cbf9ece80123824
Isaac Levy <isaac.r.levy@gmail.com>
2018-02-13 17:28:01 -0500

    speed up zeros
    Based on byte code inspection and profiling.
    
    These changes unfortunately do make a difference.

--- src/main/scala/Zero.scala
+++ src/main/scala/Zero.scala
@@ -21,20 +21,20 @@ object Zero {
   trait Zeros {
     import Zero.{instance => z}
 
-    implicit val stringZero: Zero[String] = z("")
-    implicit val booleanZero: Zero[Boolean] = z(false)
-    implicit val intZero: Zero[Int] = z(0)
-    implicit val longZero: Zero[Long] = z(0l)
-    implicit val doubleZero: Zero[Double] = z(0d)
-    implicit val floatZero: Zero[Float] = z(0f)
-    implicit val unitZero: Zero[Unit] = z(())
+    implicit final val stringZero: Zero[String] = z("")
+    implicit final val booleanZero: Zero[Boolean] = z(false)
+    implicit final val intZero: Zero[Int] = z(0)
+    implicit final val longZero: Zero[Long] = z(0l)
+    implicit final val doubleZero: Zero[Double] = z(0d)
+    implicit final val floatZero: Zero[Float] = z(0f)
+    implicit final val unitZero: Zero[Unit] = z(())
 
-    private val zList = z(List.empty)
-    private val zMap = z(Map.empty)
-    private val zOption = z(Option.empty)
-    private val zSet = z(Set.empty)
-    private val zSeq = z(Seq.empty)
-    private val zVector = z(Vector.empty)
+    private[this] val zList = z(List.empty)
+    private[this] val zMap = z(Map.empty)
+    private[this] val zOption = z(Option.empty)
+    private[this] val zSet = z(Set.empty)
+    private[this] val zSeq = z(Seq.empty)
+    private[this] val zVector = z(Vector.empty)
 
     @inline implicit def listZero[A] = zList.asInstanceOf[Zero[List[A]]]
     @inline implicit def mapZero[A, B] = zMap.asInstanceOf[Zero[Map[A, B]]]
