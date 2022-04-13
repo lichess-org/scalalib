b2104374cbd5aa7f32ba92af73b3129ba090a409
Thibault Duplessis <t@lichess.org>
2022-04-13 12:09:26 +0200

    scala3 extension for Regex

--- src/main/scala/Regex.scala
+++ src/main/scala/Regex.scala
@@ -1,18 +1,9 @@
 package ornicar.scalalib
 
-import scala.util.matching.{ Regex => ScalaRegex }
+import scala.util.matching.Regex
 
-trait Regex {
-  @inline implicit def toOrnicarRegex(r: ScalaRegex): OrnicarRegexWrapper = new OrnicarRegexWrapper(r)
-}
-
-object Regex extends Regex
-
-final class OrnicarRegexWrapper(private val r: ScalaRegex) extends AnyVal {
-
+extension (r: Regex)
   def find(s: String): Boolean =
     r.pattern.matcher(s).find
-
   def matches(s: String): Boolean =
     r.pattern.matcher(s).matches
-}
