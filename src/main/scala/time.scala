9d6fd18e88ac7bcf0a7258c93a7bd8560a4e2ad1
Thibault Duplessis <t@lichess.org>
2023-04-02 13:46:00 +0200

    9.3.0-RC2

--- src/main/scala/time.scala
+++ src/main/scala/time.scala
@@ -1,11 +1,26 @@
 package ornicar.scalalib
 
-import java.time.{ Instant, LocalDateTime, ZoneOffset }
+import java.time.{ Duration, Instant, LocalDateTime, ZoneOffset }
 import java.time.temporal.ChronoUnit
 
 object time:
 
-  extension (d: LocalDateTime) def toMillis: Long = d.toInstant(ZoneOffset.UTC).toEpochMilli
+  val utcZone = ZoneOffset.UTC
+
+  extension (d: LocalDateTime)
+    def toMillis: Long                               = d.toInstant(utcZone).toEpochMilli
+    def toSeconds: Long                              = toMillis / 1000
+    def toCentis: Long                               = toMillis / 10
+    def toNow: Duration                              = Duration.between(d, LocalDateTime.now)
+    def isBeforeNow: Boolean                         = d.isBefore(LocalDateTime.now)
+    def isAfterNow: Boolean                          = d.isAfter(LocalDateTime.now)
+    def atMost(other: LocalDateTime): LocalDateTime  = if other.isBefore(d) then other else d
+    def atLeast(other: LocalDateTime): LocalDateTime = if other.isAfter(d) then other else d
+    def withTimeAtStartOfDay: LocalDateTime          = d.toLocalDate.atStartOfDay
+    def plus(duration: scala.concurrent.duration.Duration): LocalDateTime =
+      d.plus(duration.toMillis, ChronoUnit.MILLIS)
+    def minus(duration: scala.concurrent.duration.Duration): LocalDateTime =
+      d.minus(duration.toMillis, ChronoUnit.MILLIS)
 
   def millisToDate(millis: Long): LocalDateTime =
     LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneOffset.UTC)
