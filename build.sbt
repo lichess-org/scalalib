86f0ab8356dbb54de81657f5d9b949d2a1ef2948
Thibault Duplessis <t@lichess.org>
2024-03-30 21:00:07 +0100

    more utils from lila, 10.0.1

--- build.sbt
+++ build.sbt
@@ -1,14 +1,15 @@
 lazy val scalalib = Project("scalalib", file("."))
 organization                                          := "com.github.lichess-org"
 name                                                  := "scalalib"
-version                                               := "10.0.0"
+version                                               := "10.0.1"
 scalaVersion                                          := "3.4.1"
 licenses += "MIT"                                     -> url("https://opensource.org/licenses/MIT")
 libraryDependencies += "org.typelevel"                %% "cats-core"      % "2.10.0"
 libraryDependencies += "org.typelevel"                %% "alleycats-core" % "2.10.0"
 libraryDependencies += "com.lihaoyi"                  %% "pprint"         % "0.7.0"
-libraryDependencies += "com.github.ben-manes.caffeine" % "caffeine"       % "3.1.8" % "compile"
-libraryDependencies += "com.github.blemale"           %% "scaffeine"      % "5.2.1" % "compile"
+libraryDependencies += "com.github.ben-manes.caffeine" % "caffeine"       % "3.1.8"     % "compile"
+libraryDependencies += "com.github.blemale"           %% "scaffeine"      % "5.2.1"     % "compile"
+libraryDependencies += "org.scalameta"                %% "munit"          % "1.0.0-M11" % Test
 
 scalacOptions := Seq(
   "-encoding",
---
+++ src/main/scala/HeapSort.scala
@@ -0,0 +1,41 @@
+package scalalib
+
+import scala.collection.BuildFrom
+import scala.collection.mutable.{ Growable, PriorityQueue }
+import scala.math.Ordering
+
+/*
+ * Sorts elements in priority order: higher first, lower last.
+ * It's the opposite of what scala .sort does.
+ */
+object HeapSort:
+
+  private def moveN[T](p: PriorityQueue[T], g: Growable[T], n: Int): Unit =
+    // Only the dequeue and dequeueAll methods will return elements in priority order (while removing elements from the heap).
+    var k = math.min(p.length, n)
+    while k > 0 do
+      g += p.dequeue()
+      k -= 1
+
+  /* selects maximum nb elements from n size collection
+   * should be used for small nb and large n
+   * Complexity: O(n + nb * log(n))
+   */
+  def topN[T, C](xs: IterableOnce[T], nb: Int)(using ord: Ordering[T])(using
+      bf: BuildFrom[xs.type, T, C]
+  ): C =
+    val p = PriorityQueue.from(xs)(ord)
+    val b = bf.newBuilder(xs)
+    b.sizeHint(math.min(p.length, nb))
+    moveN(p, b, nb)
+    b.result()
+
+  def topNToList[T](xs: IterableOnce[T], nb: Int)(using ord: Ordering[T]): List[T] =
+    val p = PriorityQueue.from(xs)(ord)
+    val b = List.newBuilder[T]
+    moveN(p, b, nb)
+    b.result()
+
+  extension [A](l: List[A])(using ord: Ordering[A])
+    def topN(nb: Int): List[A] = HeapSort.topNToList(l, nb)
+    def botN(nb: Int): List[A] = HeapSort.topNToList(l, nb)(using ord.reverse)
