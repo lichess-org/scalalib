9c169bf50a7ae7272ffd5accd1e21a02ea388d08
Thibault Duplessis <t@lichess.org>
2024-04-01 11:22:48 +0200

    add cache utilities

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -1,13 +1,75 @@
 package scalalib
+package cache
 
 import com.github.blemale.scaffeine.Scaffeine
 import com.github.benmanes.caffeine.cache.Scheduler
 import java.util.concurrent.Executor
+import scala.concurrent.duration.FiniteDuration
+import com.github.blemale.scaffeine.Cache
 
-object cache:
+def scaffeine(using Executor): Scaffeine[Any, Any] =
+  scaffeineNoScheduler.scheduler(Scheduler.systemScheduler)
 
-  def scaffeine(using Executor): Scaffeine[Any, Any] =
-    scaffeineNoScheduler.scheduler(Scheduler.systemScheduler)
+def scaffeineNoScheduler(using ex: Executor): Scaffeine[Any, Any] =
+  Scaffeine().executor(ex)
 
-  def scaffeineNoScheduler(using ex: Executor): Scaffeine[Any, Any] =
-    Scaffeine().executor(ex)
+final class ExpireSetMemo[K](ttl: FiniteDuration)(using Executor):
+
+  private val cache: Cache[K, Boolean] = scaffeineNoScheduler
+    .expireAfterWrite(ttl)
+    .build[K, Boolean]()
+
+  // we compare to `true` so that `null` is turned to `false`
+  def get(key: K): Boolean = cache.underlying.getIfPresent(key) == true
+
+  def intersect(keys: Iterable[K]): Set[K] =
+    if keys.nonEmpty then
+      val res = cache.getAllPresent(keys)
+      keys.filter(res.contains).toSet
+    else Set.empty
+
+  def put(key: K) = cache.put(key, true)
+
+  def putAll(keys: Iterable[K]) = cache.putAll(keys.view.map(k => k -> true).toMap)
+
+  def remove(key: K) = cache.invalidate(key)
+
+  def removeAll(keys: Iterable[K]) = cache.invalidateAll(keys)
+
+  def keys: Iterable[K] = cache.asMap().keys
+
+  def keySet: Set[K] = keys.toSet
+
+  def count: Int = cache.estimatedSize().toInt
+
+final class HashCodeExpireSetMemo[A](ttl: FiniteDuration)(using Executor):
+
+  private val cache: Cache[Int, Boolean] = scaffeineNoScheduler
+    .expireAfterWrite(ttl)
+    .build[Int, Boolean]()
+
+  def get(key: A): Boolean = cache.underlying.getIfPresent(key.hashCode) == true
+
+  def put(key: A): Unit = cache.put(key.hashCode, true)
+
+  def remove(key: A): Unit = cache.invalidate(key.hashCode)
+
+object OnceEvery:
+
+  def apply[A](ttl: FiniteDuration)(using Executor): A => Boolean =
+
+    val cache = ExpireSetMemo[A](ttl)
+
+    key =>
+      val isNew = !cache.get(key)
+      if isNew then cache.put(key)
+      isNew
+
+  def hashCode[A](ttl: FiniteDuration)(using Executor): A => Boolean =
+
+    val cache = HashCodeExpireSetMemo[A](ttl)
+
+    key =>
+      val isNew = !cache.get(key)
+      if isNew then cache.put(key)
+      isNew
