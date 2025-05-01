d82581759204b05637bd519ccf800417b2eb5271
Thibault Duplessis <t@lichess.org>
2025-05-01 10:52:21 +0200

    add explicit typing to Bus functions

--- lila/src/main/scala/Bus.scala
+++ lila/src/main/scala/Bus.scala
@@ -74,7 +74,8 @@ final class Bus(initialCapacity: Int = 4096):
       // it's not always error when type T is enum, and matching only one variant
       f.applyOrElse(x, _ => ())
     // logic error in `scalalib`, because events are based by types
-    case y => println(s"Subscribe error: Incorrect message type, wanted: ${typeName[T]}, received: $y. This is a bug, report to scalalib")
+    case y =>
+      println(s"Subscribe error: wanted ${typeName[T]}, received: $y. This is a bug, report to scalalib")
 
   // BC
   def publish = publishDyn
@@ -83,20 +84,22 @@ final class Bus(initialCapacity: Int = 4096):
     entries.unsafeMap.get(channel).foreach(_.foreach(_ ! payload))
 
   // BC
-  def subscribe(subscriber: Tellable, to: Channel*) =
+  def subscribe(subscriber: Tellable, to: Channel*): Unit =
     subscribeDyn(subscriber, to*)
 
-  def subscribeDyn(subscriber: Tellable, to: Channel*) =
+  def subscribeDyn(subscriber: Tellable, to: Channel*): Unit =
     to.foreach:
       entries.unsafeMap.compute(_): prev =>
         Some(prev.fold(Set(subscriber))(_ + subscriber))
 
   // BC
-  def subscribe(ref: scalalib.actor.SyncActor, to: Channel*) =
+  def subscribe(ref: scalalib.actor.SyncActor, to: Channel*): Unit =
     subscribeDyn(Tellable.SyncActor(ref), to*)
 
   // LOGIC : It is up to the caller to make sure `tellable` is expecting payload of type `T`
-  inline def subscribeActor[T <: Payload](ref: scalalib.actor.SyncActor)(using NotGiven[T <:< NotBuseable]) =
+  inline def subscribeActor[T <: Payload](ref: scalalib.actor.SyncActor)(using
+      NotGiven[T <:< NotBuseable]
+  ): TypedTellable[T] =
     subTellable[T](Tellable.SyncActor(ref))
 
   // BC
@@ -113,29 +116,33 @@ final class Bus(initialCapacity: Int = 4096):
     subscriptions.foreach: (channel, subscriber) =>
       subscribeFun(channel)(subscriber)
 
-  inline def unsub[T <: Payload](subscriber: TypedTellable[T])(using NotGiven[T <:< NotBuseable]) =
+  inline def unsub[T <: Payload](subscriber: TypedTellable[T])(using
+      NotGiven[T <:< NotBuseable]
+  ): Option[Set[Tellable]] =
     assertBuseable[T]
     unsubUnchecked[T](subscriber.tellable)
 
   // LOGIC : It is up to the caller to maka sure `tellable` was subscribed in `T` channel in the first place
-  inline def unsubUnchecked[T <: Payload](subscriber: Tellable)(using NotGiven[T <:< NotBuseable]) =
+  inline def unsubUnchecked[T <: Payload](subscriber: Tellable)(using
+      NotGiven[T <:< NotBuseable]
+  ): Option[Set[Tellable]] =
     assertBuseable[T]
     entries.computeIfPresent[T]: subs =>
       val subsLeft = subs - subscriber
       Option.when(subsLeft.nonEmpty)(subsLeft)
 
-  def unsubscribeDyn(subscriber: Tellable, from: Iterable[Channel]) =
+  def unsubscribeDyn(subscriber: Tellable, from: Iterable[Channel]): Unit =
     from.foreach:
       entries.unsafeMap.computeIfPresent(_): subs =>
         val newSubs = subs - subscriber
         Option.when(newSubs.nonEmpty)(newSubs)
 
   // BC
-  def unsubscribe(subscriber: Tellable, from: Channel*) =
+  def unsubscribe(subscriber: Tellable, from: Channel*): Unit =
     unsubscribeDyn(subscriber, from)
 
   // BC
-  def unsubscribe(subscriber: Tellable, from: Iterable[Channel]) =
+  def unsubscribe(subscriber: Tellable, from: Iterable[Channel]): Unit =
     unsubscribeDyn(subscriber, from)
 
   // BC
@@ -172,4 +179,4 @@ final class Bus(initialCapacity: Int = 4096):
   private[scalalib] val entries: MutableTypeMap[Set[Tellable], ConcurrentMap.Backend] =
     MutableTypeMap.make(initialCapacity)
 
-  def size() = entries.unsafeMap.size()
+  def size(): Int = entries.unsafeMap.size()
